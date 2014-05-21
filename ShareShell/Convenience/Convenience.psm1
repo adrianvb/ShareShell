17f5e02180b156b3ad7bb708181cd5dd29365853
adrianvb <github@netzkind.org>
2014-05-21 13:38:29 +0200

    improve web uri detection

--- ShareShell/Convenience/Convenience.psm1
+++ ShareShell/Convenience/Convenience.psm1
@@ -3,9 +3,20 @@
 		[Parameter(Mandatory=$true)] [String] $Uri
 	)
 	
+	
+	
+	# https://sharepoint/sites/playground/_layouts/15/start.aspx#/Lists/Reboot/AllItems.aspx
+	if ($Uri -like "*/_layouts/15/start.aspx#*") {
+		$Uri = $Uri.Replace("_layouts/15/start.aspx#/", "")
+	}
+	
+	if ($Uri -match "/Lists/.*|/SiteAssets/.*") {
+		$Uri = $Uri.Replace($Matches[0], "")
+	}
+	
 	if ($Uri -notlike "*/_api/web*") {
 		$Uri = "$Uri/_api/web"
-	}
+	}	
 	
 	Invoke-XmlApiRequest -Uri $Uri	
 }
