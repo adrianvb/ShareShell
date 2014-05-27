5e4f012761abfc5b7a597d7ce8670c1bcc2e2b9e
adrianvb <github@netzkind.org>
2014-05-27 15:49:40 +0200

    New module "Utility"
    added some simple caching mechanism

---
+++ ShareShell/Utility/Utility.psm1
@@ -0,0 +1,23 @@
+﻿
+Function Get-CachedItem {
+	Param(
+		$Key
+	)
+	
+	$Global:ShareShellCache[$Key]
+}
+
+Function Add-CachedItem {
+	Param(
+		$Key,
+		$Value
+	)
+	
+	$Global:ShareShellCache[$Key] = $Value
+}
+
+Function Clear-Cache {
+	$Global:ShareShellCache = @{}
+}
+
+Clear-Cache
