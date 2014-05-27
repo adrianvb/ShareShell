6e521b99ed90fe739d598902e295761d24aff62d
adrianvb <github@netzkind.org>
2014-05-27 15:50:04 +0200

    Form Digest Tokens are now cached
    using Utility\Utility.psm1

--- ShareShell/API/API.psm1
+++ ShareShell/API/API.psm1
@@ -323,11 +323,34 @@ Function Get-FormDigest {
 	Param (
 		[String] $BaseUri
 	)
-	$Response = Invoke-WebRequest -Method  Post -Uri "$BaseUri/_api/contextinfo" -UseDefaultCredentials
-	[Xml] $Content = $Response.Content
 	
-	$Content.GetContextWebInformation.FormDigestValue
+	$Uri =  "$BaseUri/_api/contextinfo"
+
+	# fetch cached xml
+	$Token = Get-CachedItem -Key $Uri
+	
+	# if cached xml is $null, force reload
+	$Reload = $Token -eq $null
+	# only reload if token is not valid anymore
+	if (-not $Reload) {
+		$Reload = $Token["Timeout"] -lt (Get-Date)
+	}
+	
+	if ($Reload) {
+		$Response = Invoke-WebRequest -Method  Post -Uri $Uri -UseDefaultCredentials
+		[Xml] $Data = $Response.Content
+		
+		$Token = @{
+			"Timeout" = (Get-Date).AddSeconds($Data.GetContextWebInformation.FormDigestTimeoutSeconds."#text")
+			"FormDigest" = $Data.GetContextWebInformation.FormDigestValue
+		}
+		
+		Add-CachedItem -Key $Uri -Value $Token
+	}
+	
+	$Token["FormDigest"]
 }
+
 
 Set-StrictMode -Version Latest
 #Export-ModuleMember ("Invoke-XmlApiRequest", "New-ListItem")
