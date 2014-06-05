911061a269b7c71c7e977586cd5dc6d77aae3e08
adrianvb <github@netzkind.org>
2014-06-05 11:24:47 +0200

    moved Get-FormDigest to Utility.psm1

--- ShareShell/API/API.psm1
+++ ShareShell/API/API.psm1
@@ -229,39 +229,6 @@ Function New-ListItem {
 	$Item
 }
 
-
-Function Get-FormDigest {
-	Param (
-		[String] $BaseUri
-	)
-	
-	$Uri =  "$BaseUri/_api/contextinfo"
-
-	# fetch cached xml
-	$Token = Get-CachedItem -Key $Uri
-	
-	# if cached xml is $null, force reload
-	$Reload = $Token -eq $null
-	# only reload if token is not valid anymore
-	if (-not $Reload) {
-		$Reload = $Token["Timeout"] -lt (Get-Date)
-	}
-	
-	if ($Reload) {
-		$Response = Invoke-WebRequest -Method  Post -Uri $Uri -UseDefaultCredentials
-		[Xml] $Data = $Response.Content
-		
-		$Token = @{
-			"Timeout" = (Get-Date).AddSeconds($Data.GetContextWebInformation.FormDigestTimeoutSeconds."#text")
-			"FormDigest" = $Data.GetContextWebInformation.FormDigestValue
-		}
-		
-		Add-CachedItem -Key $Uri -Value $Token
-	}
-	
-	$Token["FormDigest"]
-}
-
 Function Add-ApiMethod {
 	Param(
 		[Parameter(Mandatory=$true)] [ValidateSet("Create","Update","Delete")] [String] $Operation,
--- ShareShell/Utility/Utility.psm1
+++ ShareShell/Utility/Utility.psm1
@@ -1,4 +1,37 @@
 ﻿
+Function Get-FormDigest {
+	Param (
+		[String] $BaseUri
+	)
+	
+	$Uri =  "$BaseUri/_api/contextinfo"
+
+	# fetch cached xml
+	$Token = Get-CachedItem -Key $Uri
+	
+	# if cached xml is $null, force reload
+	$Reload = $Token -eq $null
+	# only reload if token is not valid anymore
+	if (-not $Reload) {
+		$Reload = $Token["Timeout"] -lt (Get-Date)
+	}
+	
+	if ($Reload) {
+		$Response = Invoke-WebRequest -Method  Post -Uri $Uri -UseDefaultCredentials
+		[Xml] $Data = $Response.Content
+		
+		$Token = @{
+			"Timeout" = (Get-Date).AddSeconds($Data.GetContextWebInformation.FormDigestTimeoutSeconds."#text")
+			"FormDigest" = $Data.GetContextWebInformation.FormDigestValue
+		}
+		
+		Add-CachedItem -Key $Uri -Value $Token
+	}
+	
+	$Token["FormDigest"]
+}
+
+
 Function Get-CachedItem {
 	Param(
 		$Key
